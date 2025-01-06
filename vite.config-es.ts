c66dc2ed0a2da9b244c4f762bea131994ec05297
Sebastian <sebastian@stryczek.pl>
2025-01-06 16:34:34 +0100

    Fix release config

--- vite.config-es.ts
+++ vite.config-es.ts
@@ -4,38 +4,30 @@ import dts from 'vite-plugin-dts';
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const plugins = [
-  dts({insertTypesEntry: true}),
-];
-
 export default defineConfig({
   mode: isProduction ? "production" : "development",
   build: {
     minify: isProduction,
-
-    sourcemap: !isProduction,
+    sourcemap: true,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/leaflet-maptilersdk.ts'),
       name: 'leafletmaptilersdk',
-      // the proper extensions will be added
-      fileName: (format, entryName) => "leaflet-maptilersdk.js",
+      fileName: () => "leaflet-maptilersdk.min.js",
       formats: ['es'],
     },
-    
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
+      // make sure to externalize deps that shouldn't be bundled into your library
       external: [
         "leaflet",
         "@maptiler/sdk",
       ],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
+        // Provide global variables to use in the UMD build for externalized deps
         globals: {},
       },
     },
   },
-  plugins,
-})
+  plugins: [
+    dts({insertTypesEntry: true}),
+  ],
+});
